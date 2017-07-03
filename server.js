f430925927a27352c8305bc0ef40fcd0af418ca8
sairam2093 <sai.adusumilli@gmail.com>
2017-07-02 20:14:00 -0500

    adding Put and delete

--- server.js
+++ server.js
@@ -51,9 +51,55 @@ app.post('/todos', function (req,res) {
 			return res.sendStatus(400);
 		}
 	body.description = body.description.trim();
+	
 	todos.push(body);
 		
 	res.json(body);
+
+});
+
+//DELETE: /todos/:id
+app.delete('/todos/:id', function (req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var todoItem = _.findWhere(todos, {id: todoId});
+	if(!todoItem){
+		res.status(400).json({"error": "No todo Item Found with that Id"});
+	}else{
+	 todos = _.without(todos, todoItem);
+	 res.json(todoItem);
+	}
+
+});
+
+//PUT : /todos/:id
+app.put('/todos/:id', function (req, res){
+	var body = _.pick(req.body,'description','completed');
+	var todoId = parseInt(req.params.id, 10);
+	var matchedItem = _.findWhere(todos, {id: todoId});
+	var validAttributes = {};
+
+		if(!matchedItem){
+			res.send(404).json({"error": "Not found with that id "});
+		}
+		
+		if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+			validAttributes.completed = body.completed;
+		//	matchedItem.completed = validAttributes.completed;
+		}
+		else if (body.hasOwnProperty('completed')){
+			return res.status(400).json({"error": "Something wrong with completed"});
+		}
+
+		if(body.hasOwnProperty('description') && _.isString(body.description) && 
+				body.description.trim().length > 0){
+			validAttributes.description = body.description;
+		//	matchedItem.description = validAttributes.description;
+		}
+		else if (body.hasOwnProperty('description')){
+			return res.status(400).json({"error": "Something wrong with description"});
+		}
+		_.extend(matchedItem,validAttributes);
+		res.json(matchedItem);
 
 });
 
